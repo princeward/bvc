c57f42add2c69c1b4175eda2483964001494c683
princeward <princeward.bu@gmail.com>
2017-01-25 18:09:34 -0800

    added the first script

---
+++ src/test.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+class BVC:
+	def __init__(self, own_pos, nbr_pos, world_corners, world_edges):
+		pass
+
+	def get_bvc(self):
+		ver = world_corner # initial BVC cell
+
+	def get_line(self):
+
+
